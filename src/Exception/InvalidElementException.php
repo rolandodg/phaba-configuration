24c7244eea6325cf44637c107c0e0d6bed5b2491
rolandodg <jesushs80@gmail.com>
2017-04-02 12:43:40 +0200

    Created documentation for exceptions.

--- src/Exception/InvalidElementException.php
+++ src/Exception/InvalidElementException.php
@@ -6,6 +6,11 @@ namespace Phaba\Configuration\Exception;
 
 use Exception;
 
+/**
+ * Thrown exception when configuration element isn't found.
+ *
+ * @package Phaba\Configuration\Exception
+ */
 class InvalidElementException extends Exception
 {
 
